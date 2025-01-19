5e213dc4c60f8c36edfa1452134ab1e2233131c5
zgardner008 <zgardner007@gmail.com>
2025-01-19 00:59:07 -0600

    Add player creation and draw player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+import player
 from constants import *
 
 
@@ -9,12 +10,16 @@ def main():
         f"Starting asteroids!\nScreen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}"
     )
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_instance = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(0x000000)
+        player_instance.draw(screen)
         pygame.display.flip()
+        Clock = pygame.time.Clock()
+        dt = Clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
